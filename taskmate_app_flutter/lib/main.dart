ab097f064a8f846336c4570446c81b6f37532339
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-01-27 23:26:09 +0600

    Initialize Hive and open settings box in main

--- taskmate_app_flutter/lib/main.dart
+++ taskmate_app_flutter/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'features/tasks/ui/task_home_page.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('settings_box');
   runApp(const TaskMateApp());
 }
+
 class TaskMateApp extends StatelessWidget {
   const TaskMateApp({super.key});
   @override
