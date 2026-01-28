1bfbcaddc6a3574fbc5788cba4e7f876dbbcaf5b
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-01-28 21:41:38 +0600

    main

--- taskmate_app_flutter/lib/main.dart
+++ taskmate_app_flutter/lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'features/tasks/ui/task_home_page.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('settings_box');
   runApp(const TaskMateApp());
 }
-
 class TaskMateApp extends StatelessWidget {
   const TaskMateApp({super.key});
-  
   @override
   Widget build(BuildContext context) {
     final colorScheme = ColorScheme.fromSeed(seedColor: Colors.indigo);
