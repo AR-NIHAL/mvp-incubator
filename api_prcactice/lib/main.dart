70176e8484d0579ed4189bc48ecafcb7012524bb
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-03-09 21:37:21 +0600

    Update main.dart

--- api_prcactice/lib/main.dart
+++ api_prcactice/lib/main.dart
@@ -1,18 +1,1 @@
-import 'package:api_prcactice/home_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(ProviderScope(child: MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
